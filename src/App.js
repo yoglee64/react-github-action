4e3b41ac9b345fa10aebe4b970967d281a271a79
yoglee64 <l3060921@naver.com>
2024-03-16 00:22:57 +0900

    initial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setcount] = useState(0);
+  const [disabled, setdisabled] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h3 data-testid="counter">{count}</h3>
+        <div>
+          <button
+            data-testid="minus-button"
+            disabled={disabled}
+            onClick={() => setcount((prev) => prev - 1)}
+          >
+            -
+          </button>
+          <button
+            data-testid="plus-button"
+            disabled={disabled}
+            onClick={() => setcount((prev) => prev + 1)}
+          >
+            +
+          </button>
+        </div>
+        <div>
+          <button
+            style={{ backgroundColor: "blue" }}
+            data-testid= "on/off-button"
+            onClick={() => setdisabled((prev) => !prev)}
+          >
+            on/off
+          </button>
+        </div>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,62 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('the counter starts at 0', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(0);
 });
+test("minus button has correct text", ()=>{
+  // APP 컴포넌트를 렌더링함
+  render(<App />);
+  // screen object를 이용해서 원하는 엘리먼트에 접근(접근 시 id로 접근)
+  const buttonElement = screen.getByTestId("minus-button");
+  // id가 counter인 엘리먼트가 0인지 테스트
+  expect(buttonElement).toHaveTextContent("-");
+});
+test("plus button has correct text", ()=>{
+  // App 컴포넌트를 렌더링함
+  render(<App />);
+  // screen object를 이용해서 원하는 엘리먼트에 접근 (접근 시 id로 접근)
+  const buttonElement = screen.getByTestId("plus-button");
+  // id가 counter인 엘리먼트가 0인지 테스트
+  expect(buttonElement).toHaveTextContent("+");
+})
+test("When the + button is pressed, the counter changes to 1", () => {
+  // App컴포넌트를 렌더링함
+  render(<App />);
+  // screen object를 이용해서 원하는 엘리먼트에 접근(접근 시 id로 접근)
+  const buttonElement = screen.getByTestId("plus-button");
+  // click plus button
+  fireEvent.click(buttonElement);
+  // 카운터가 0에서 +되어 1이 됨
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(1);
+})
+test("When the - button is pressed, the counter changes to -1", () => {
+  // App컴포넌트를 렌더링함
+  render(<App />);
+  // screen object를 이용해서 원하는 엘리먼트에 접근(접근 시 id로 접근)
+  const buttonElement = screen.getByTestId("minus-button");
+  // click minus button
+  fireEvent.click(buttonElement);
+  // 카운터가 0에서 -되어 -1이 됨
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(-1);
+})
+test("on/off button has blue color",()=>{
+  // App 컴포넌트를 렌더링함
+  render(<App />);
+  // screen object를 이용해서 원하는 엘리먼트에 접근(접근 시 id로 접근)
+  const buttonElement = screen.getByTestId("on/off-button");
+  // id가 counter인 엘리먼트가 0인지 테스트
+  expect(buttonElement).toHaveStyle({backgroundColor : "blue"});
+})
+test("Prevent the -,+ button from being pressed when the on/off button is clicked",()=>{
+  render(<App />);
+  const onOffButtonElement = screen.getByTestId("on/off-button");
+  // click onOffButtonElement button
+  fireEvent.click(onOffButtonElement);
+  const plusButtonElement = screen.getByTestId("plus-button");
+  expect(plusButtonElement).toBeDisabled();
+})
